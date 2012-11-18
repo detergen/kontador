ddee7972c63e2550224077966355e86c3244498a
Alex Tikhomirov <alex@swimming.ru>
2012-11-18 14:15:13 +0400

    Remove payments from project at all

--- app/models/payment.rb
+++
@@ -1,8 +0,0 @@
-class Payment < ActiveRecord::Base
-	belongs_to :order
-
-	belongs_to :fromacc, :class_name => "Bankacc", :foreign_key => 'fromacc_id'
-	belongs_to :toacc, :class_name => "Bankacc", :foreign_key => 'toacc_id'
-
-
-end
